1762ef5447faa4f1518106de7673e5bc7f4554c8
tianxuewei1016 <93226539@qq.com>
2017-06-22 19:34:01 +0800

    全部理财页面完成

---
+++ app/src/main/java/com/p2ptwo0224/adapter/InvestAllAdapter.java
@@ -0,0 +1,89 @@
+package com.p2ptwo0224.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.p2ptwo0224.R;
+import com.p2ptwo0224.bean.InvestAllBean;
+import com.p2ptwo0224.view.MyProgress;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * 作者：田学伟 on 2017/6/22 19:08
+ * QQ：93226539
+ * 作用：
+ */
+
+public class InvestAllAdapter extends BaseAdapter {
+
+    private List<InvestAllBean.DataBean> list = new ArrayList<>();
+
+    public InvestAllAdapter(List<InvestAllBean.DataBean> list) {
+        if (list != null && list.size() > 0) {
+            this.list.clear();
+            this.list.addAll(list);
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder = null;
+        if (convertView == null) {
+            convertView = View.inflate(parent.getContext(), R.layout.adapter_invest_all, null);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        }else{
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        InvestAllBean.DataBean dataBean = list.get(position);
+
+        viewHolder.pName.setText(dataBean.getName());
+
+        return convertView;
+    }
+
+
+    class ViewHolder {
+        @Bind(R.id.p_name)
+        TextView pName;
+        @Bind(R.id.p_money)
+        TextView pMoney;
+        @Bind(R.id.p_yearlv)
+        TextView pYearlv;
+        @Bind(R.id.p_suodingdays)
+        TextView pSuodingdays;
+        @Bind(R.id.p_minzouzi)
+        TextView pMinzouzi;
+        @Bind(R.id.p_minnum)
+        TextView pMinnum;
+        @Bind(R.id.p_progresss)
+        MyProgress pProgresss;
+
+        ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
+}
--- app/src/main/java/com/p2ptwo0224/fragment/InvestAllFragment.java
+++ app/src/main/java/com/p2ptwo0224/fragment/InvestAllFragment.java
@@ -1,7 +1,15 @@
 package com.p2ptwo0224.fragment;
 
+import android.widget.ListView;
+
+import com.alibaba.fastjson.JSON;
 import com.p2ptwo0224.R;
+import com.p2ptwo0224.adapter.InvestAllAdapter;
 import com.p2ptwo0224.base.BaseFragment;
+import com.p2ptwo0224.bean.InvestAllBean;
+import com.p2ptwo0224.common.AppNetConfig;
+
+import butterknife.Bind;
 
 /**
  * 作者：田学伟 on 2017/6/22 18:23
@@ -9,7 +17,10 @@ import com.p2ptwo0224.base.BaseFragment;
  * 作用：
  */
 
-public class InvestAllFragment extends BaseFragment{
+public class InvestAllFragment extends BaseFragment {
+    @Bind(R.id.invest_all_lv)
+    ListView investAllLv;
+
     @Override
     protected void initListener() {
 
@@ -17,7 +28,9 @@ public class InvestAllFragment extends BaseFragment{
 
     @Override
     protected void initData(String json) {
-
+        InvestAllBean investAllBean = JSON.parseObject(json, InvestAllBean.class);
+        InvestAllAdapter adapter = new InvestAllAdapter(investAllBean.getData());
+        investAllLv.setAdapter(adapter);
     }
 
     @Override
@@ -27,6 +40,7 @@ public class InvestAllFragment extends BaseFragment{
 
     @Override
     public String getChildUrl() {
-        return null;
+        return AppNetConfig.PRODUCT;
     }
+
 }
